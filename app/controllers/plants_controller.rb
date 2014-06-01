3ab595a616f5d54c055d009f230226d7785efb5f
Oscar Rendon <orendon@gmail.com>
2014-06-01 00:55:11 -0400

    Add basic API structure for plant data sync

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,16 @@
 class PlantsController < ApplicationController
+  before_filter :get_plant, only: :index
+
   def index
+  end
+
+  def sync
+    render text: 'oki doki'
+  end
+
+  private
+
+  def get_plant
+    @plant = Plant.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'plants#index'
+
+  post 'plant/:id/sync' => 'plants#sync', as: :plant_sync
 end
+
