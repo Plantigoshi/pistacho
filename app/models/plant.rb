7f8cd60b57efdfb06ae595a8fb0e429f9728ce95
Oscar Rendon <orendon@gmail.com>
2014-06-01 00:07:40 -0400

    Add scopes for each plant variable

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,7 @@
 class Plant < ActiveRecord::Base
+  has_many :metrics
 
+  scope :temperatures,   ->{ where(type: 'Temperatures') }
+  scope :humidities,     ->{ where(type: 'Humidity') }
+  scope :precipitations, ->{ where(type: 'Precipitation') }
 end
