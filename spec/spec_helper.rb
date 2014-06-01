16d2994a21356932a34286c42da44c84b027559f
Oscar Rendon <orendon@gmail.com>
2014-06-01 00:29:51 -0400

    Remove duplicated config on spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,8 +13,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-RSpec.configure do |config|
-  ActiveRecord::Migration.maintain_test_schema!
+RSpec.configure do |config| 
 
   # ## Mock Framework
   #
